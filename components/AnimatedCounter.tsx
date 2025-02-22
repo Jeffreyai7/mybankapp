f88f4721a7745aecababbf2e966d243b2f9ee10e
jeffreyai7 <jirukeh@gmail.com>
2025-02-22 13:08:00 +0100

    implement chartjs shtructure

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-      <CountUp end={amount} prefix="$" decimal="," decimals={2} />
+      <CountUp end={amount} prefix="$" decimal="." decimals={2} />
     </div>
   );
 };
