252c912897b045e219f056d5eeb19f4c3064c108
jeffreyai7 <jirukeh@gmail.com>
2025-02-24 15:27:16 +0100

    implement mobile nav structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNavBar from "@/components/MobileNavBar";
 import SideBar from "@/components/SideBar";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +10,16 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
       <SideBar />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src={"/icons/logo.svg"} alt="logo" width={30} height={30} />
+          <div>
+            <MobileNavBar user={loggedIn} />
+          </div>
+        </div>
 
-      {children}
+        {children}
+      </div>
     </main>
   );
 }
---
+++ components/MobileNavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileNavBar = ({ user }: MobileNavProps) => {
+  return <div>MobileNavBar</div>;
+};
+
+export default MobileNavBar;
